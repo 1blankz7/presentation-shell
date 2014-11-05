64251c39a2dcebbaac1b52973f2c21c3fafb01ff
Christian Blank <christian.blank@haw-hamburg.de>
2014-11-05 23:34:53 +0100

    Create jquery plugin and refactor some methods.

--- scripts/script.js
+++ scripts/script.js
@@ -1,103 +1,48 @@
+(function($) {
+    $.fn.shell = function(options) {
+        var opt = $.extend(true, {}, $.fn.shell.defaults, options),
+            $this = $(this),
+            history = new Array(),
+            history_idx = 0,
+            write_idx = 0,
+            current_line = "",
+            input = null,
+            prompt_line = null,
+            history_window = null;
 
-var Shell = function(opt) {
-    var defaults = {
-        server_url: '',
-        prompt: '$> '
-    };
-    var history = new Array();
-    var history_idx = 0;
-    var write_idx = 0;
-    var current_line = "";
-    var input = null;
-    var prompt_line = null;
-    var history_window = null;
+        var call = function(cmd, callback) {
+            // create request
+            $.post(opt.server_url, {'cmd': cmd}, function(data) {
+                callback(data);
+            });
+        };
 
-    /**
-     * Returns the current working directory of the server
-     */
-    this.cwd = function(callback) {
-        var cmd = 'pwd';
-        return call(pwd, callback);
-    };
-
-    var call = function(cmd, callback) {
-        // create request
-        $.post('/', {'cmd': cmd}, function(data) {
-            callback(data);
-        });
-    };
-
-    var send_to_server = function() {
-        var cmd = current_line;
-        current_line = "";
-        
-        history.push(cmd);
-
-        call(cmd, function(result) {
-            var old_cmd = history[history.length-1];
-            $('.prompt', prompt_line).text(result.cwd+" "+defaults.prompt);
-            var processed_result = result.result.replace(/(?:\r\n|\r|\n)/g, '<br />');
-            var el = '<p><span class="cmd">'+defaults.prompt+old_cmd+'</span><br>'+processed_result+'</p>';
-            
-            history_window.append(el);
-            history_window.scrollTop(history_window[0].scrollHeight);
-        });
-    };
-
-    this.init = function() {
-        input = $('.cmd input');
-        prompt_line = $('.prompt-line');
-        history_window = $('.history');
-
-        // add event handler to input
-        input.keydown(function(e) {
+        var onkeydown = function(e) {
             e.preventDefault();
             e.stopPropagation();
-            e.cancelBubble = true
+            e.cancelBubble = true;
             
             switch(e.which) {
                 case 8: // backspace
-                    current_line = current_line.slice(0, write_idx>0?write_idx-1:0) 
-                        + current_line.slice(write_idx);
-                    write_idx--;
-                    if(write_idx < 0) {
-                        write_idx = 0;
-                    }
+                    backspace(e.key);
                     break;
                 case 13: // enter
-                    // send to server
-                    console.log('enter')
-                    send_to_server();
+                    enter(e.key);
                     break;
                 case 37: // left
-                    write_idx--;
-                    if(write_idx < 0) {
-                        write_idx = 0;
-                    }
+                    left(e.key);
                     break;
                 case 38: // up
-                    history_idx--;
-                    if(history_idx < 0) {
-                        history_idx = 0;
-                    }
-                    current_line = history[history_idx];
+                    up(e.key);
                     break;
                 case 39: // right
-                    write_idx++;
-                    if(write_idx > current_line.length) {
-                        write_idx = current_line.length;
-                    }
+                    right(e.key);
                     break;
                 case 40: // down
-                    history_idx++;
-                    if(history_idx < history.length) {
-                        current_line = history[history_idx];
-                    } else {
-                        history_idx = history.length-1;
-                    }
+                    down(e.key);
                     break;
                 case 27: // esc
-                    input.blur();
+                    esc(e.key);
                     break;
                 case 224:
                 case 18:
@@ -107,20 +52,138 @@ var Shell = function(opt) {
                 case 20:
                     break;
                 default:
-                    current_line = current_line.slice(0, write_idx) 
-                        + e.key + current_line.slice(write_idx);
-                    write_idx++;
+                    key_default(e.key);
             }
             $(this).val(current_line);
-        });
+            input.setCursorPosition(write_idx);
+        }
 
-        // delete value in shell
-        input.val("");
+        var send_to_server = function() {
+            var cmd = current_line;
+            current_line = "";
+            
+            history.push(cmd);
 
-        return false;
+            call(cmd, function(data) {
+                var old_cmd = history[history.length-1];
+                set_prompt(data.cwd.result);
+                var processed_result = nl_to_br(data.result.result);
+                var el = '<p><span class="cmd">'+opt.prompt+old_cmd+'</span><br>'+processed_result+'</p>';  
+                history_window.append(el);
+                history_window.scrollTop(history_window[0].scrollHeight);
+            });
+        };
+
+        var nl_to_br = function(text) {
+            return text.replace(/(?:\r\n|\r|\n)/g, '<br />');
+        };
+
+        var set_prompt = function(cwd) {
+            $('.prompt', prompt_line).text(cwd+" "+opt.prompt);
+        };
+
+        var backspace = function(key) {
+            current_line = current_line.slice(0, write_idx>0?write_idx-1:0) 
+                + current_line.slice(write_idx);
+            write_idx--;
+            if(write_idx < 0) {
+                write_idx = 0;
+            }
+        };
+
+        var enter = function(key) {
+            send_to_server();
+            history_idx = history.length;
+            write_idx = current_line.length;
+        };
+
+        var left = function(key) {
+            write_idx--;
+            if(write_idx < 0) {
+                write_idx = 0;
+            }
+        };
+
+        var up = function(key) {
+            history_idx--;
+            if(history_idx < 0) {
+                history_idx = 0;
+                current_line = history.length > 0 ? history[history_idx] : current_line;
+            } else {
+                current_line = history[history_idx];
+            }
+            write_idx = current_line.length;
+        };
+
+        var right = function(key) {
+            write_idx++;
+            if(write_idx > current_line.length) {
+                write_idx = current_line.length;
+            }
+        };
+
+        var down = function(key) {
+            history_idx++;
+            if(history_idx < history.length) {
+                current_line = history[history_idx];
+            } else {
+                history_idx = history.length;
+                current_line = "";
+            }
+            write_idx = current_line.length;
+        };
+
+        var esc = function(key) {
+            write_idx = current_line.length
+            input.blur();
+        };
+
+        var key_default = function(key) {
+            current_line = current_line.slice(0, write_idx) 
+                + key + current_line.slice(write_idx);
+            write_idx++;
+        };
+
+        var init = function() {
+            $this.html($.fn.shell.markup);
+            input = $('.cmd input', $this);
+            prompt_line = $('.prompt-line', $this);
+            history_window = $('.history', $this);
+            // add event handler to input
+            input.keydown(onkeydown);
+            // delete value in shell
+            input.val("");
+
+            return false;
+        };
+
+        init();
+        return this;
     };
-} 
 
+    $.fn.shell.defaults = {
+        server_url: '/',
+        prompt: '$> '
+    };
+    $.fn.shell.markup = '<div><div class="history"></div><div class="prompt-line"><div class="prompt"></div><div class="cmd"><input type="text"></div></div></div>'
+
+    $.fn.setCursorPosition = function(pos) {
+        this.each(function(index, elem) {
+            if (elem.setSelectionRange) {
+                elem.setSelectionRange(pos, pos);
+            } else if (elem.createTextRange) {
+                var range = elem.createTextRange();
+                range.collapse(true);
+                range.moveEnd('character', pos);
+                range.moveStart('character', pos);
+                range.select();
+            }
+        });
+        return this;
+    };
+
+})(jQuery);
+    
 function fullScreen() {
     document.fullscreenEnabled = document.fullscreenEnabled ||
         document.mozFullScreenEnabled ||
@@ -141,6 +204,5 @@ function fullScreen() {
 }
 
 (function($) {
-    var shell = new Shell();
-    shell.init();
+    $('div.shell').shell();
 })(jQuery);
--- shell.py
+++ shell.py
@@ -10,19 +10,24 @@ class Shell:
 
     def execude(self, command):
         """
-        Execudes the given command. Also
-        filters requests, that contains a 'sudo' command. Returns the result
-        after call is completed.
+        Execudes the given command. Also filters requests, that contains a
+        'sudo' command. Returns the result after call is completed.
+
+        TODO: check if error occur and provide this information in result
         """
         if "sudo" in command:
-            return None
-        command = command.split()
-        if command[0] == 'cd':
-            os.chdir(command[1])
-            self.pwd = self.execude("pwd")
+            result = {'error': 'root is not supported'}
         else:
-            proc = subprocess.Popen(command, stdout=subprocess.PIPE)
-            return proc.stdout.read().decode("utf-8")
+            command = command.split()
+            if command[0] == 'cd':
+                os.chdir(command[1])
+                self.pwd = self.execude("pwd")
+                result = {'result': ""}
+            else:
+                proc = subprocess.Popen(command, stdout=subprocess.PIPE)
+                res = proc.stdout.read().decode("utf-8")
+                result = {'result': res}
+        return result
 
     def get_prompt(self):
         return (self.prompt % self.pwd[:-1])
@@ -30,8 +35,10 @@ class Shell:
     def run(self):
         user_input = input(self.get_prompt())
         result = self.execude(user_input)
-        if result is not None:
-            print(result)
+        if 'error' in result:
+            print("ERROR: %s" % result.error)
+        else:
+            print(result.result)
         self.run()
 
 
