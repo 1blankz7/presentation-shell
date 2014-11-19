f6865df4fdd821b51ce1f28733b96eabdfe918dc
Christian Blank <christian.blank@haw-hamburg.de>
2014-11-19 22:32:17 +0100

    Server now can be suspended.

--- Makefile
+++ Makefile
@@ -1,10 +1,14 @@
-PORT=1338
+PORT=1337
 PAGE_WIDTH=21.093333333cm
+PAGE_HEIGHT=13.217cm
+INPUT=index.html
+OUTPUT_NAME=index
+
 
 all: run
 
 pdf:
-	wkhtmltopdf -T 0 -R 0 -B 0 -L 0 --page-width 21.093333333cm --page-height 13.217cm --print-media-type index.html index.pdf
+	wkhtmltopdf -T 0 -R 0 -B 0 -L 0 --page-width $(PAGE_WIDTH) --page-height $(PAGE_HEIGHT) --print-media-type $(INPUT) $(OUTPUT_NAME).pdf
 
 run:
 	python3 server.py $(PORT)
--- server.py
+++ server.py
@@ -27,14 +27,42 @@ class ServerHandler(http.server.SimpleHTTPRequestHandler):
         s = json.dumps({'result': message, 'cwd': cwd})
         self.wfile.write(bytes(s, 'UTF-8'))
 
+    def do_QUIT(self):
+        """send 200 OK response, and set server.stop to True"""
+        self.send_response(200)
+        self.end_headers()
+        self.server.stop = True
+
+
+class StoppableHttpServer(http.server.HTTPServer):
+    """http server that reacts to self.stop flag"""
+    STOP = False
+
+    def serve_forever(self):
+        """Handle one request at a time until stopped."""
+        StoppableHttpServer.STOP = False
+        while not StoppableHttpServer.STOP:
+            self.handle_request()
+
 
 if __name__ == '__main__':
+    import signal
     import sys
+
+    def signal_handler(sig, frame):
+        print('You pressed Ctrl+C!')
+        StoppableHttpServer.STOP = False
+        print('Server shutdown!')
+        sys.exit(0)
+
     if len(sys.argv) > 1:
         PORT = int(sys.argv[1])
     else:
         PORT = 1338
-    Handler = ServerHandler
 
-    httpd = socketserver.TCPServer(("", PORT), Handler)
+    Handler = ServerHandler
+    httpd = StoppableHttpServer(("127.0.0.1", PORT), Handler)
+    print("Server serves on %s:%s" % httpd.server_address)
+    signal.signal(signal.SIGINT, signal_handler)
+    print('Press Ctrl+C')
     httpd.serve_forever()
